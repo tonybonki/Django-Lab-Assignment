a01c736ed59b4c0274a80c54ae9431aecf56f6f9
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-09-18 12:34:08 +0100

    created user model and fixed code

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,12 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
-
+BASE_DIR = Path(__file__).resolve().parent.parent
 MEDIA_ROOT  = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+AUTH_USER_MODEL = 'mikeshop.User'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
