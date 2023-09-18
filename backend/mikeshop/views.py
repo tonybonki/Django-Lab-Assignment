f8711f2bcca0eacbc03d322e906d295d702cdabf
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-09-18 12:52:23 +0100

    reset

--- backend/mikeshop/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-def index(request):
-    products = ['apple','banana','grapes']
-    context = {'products': products}
-    return render(request, 'index.html', context)
