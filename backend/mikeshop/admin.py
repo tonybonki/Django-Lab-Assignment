79ee095336e6a5fb5263fb0a22b2c56183c9495f
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-09-19 10:54:29 +0100

    added more models in the admin page

--- backend/mikeshop/admin.py
+++ backend/mikeshop/admin.py
@@ -4,5 +4,7 @@ from .models import *
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Product)
-
+admin.site.register(Basket)
+admin.site.register(BasketItem)
+admin.site.register(Order)
 # ...the rest of the models 
