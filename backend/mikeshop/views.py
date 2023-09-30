80f14e7728f8f9ac2c6cedf387093a09a8f9e53c
Tony Bonki <tony.bonki2@mail.dcu.ie>
2023-09-30 22:39:51 +0100

    styled form

--- backend/mikeshop/views.py
+++ backend/mikeshop/views.py
@@ -14,36 +14,6 @@ from rest_framework.decorators import api_view
 from django.http import JsonResponse
 from rest_framework import status
 
-
-# decorator from the Django REST framework
-
-
-def index(request):
-    # Your logic to fetch data and serialize it goes here
-    # Retrieve the products and categories querysets
-
-    products = Product.objects.all()
-    products_list = [
-        {
-         "id": product.id,
-         "name": product.name,
-         'price': product.price,
-         'description': product.description,
-         'image_url': product.product_image.url,
-         }
-         
-        for product in products
-        
-    ]
-
-    data = {
-        "products": products_list,
-
-    }
- 
-    # Return the combined data as a JSON response
-    return JsonResponse(data, status=status.HTTP_200_OK)
-
 # Django Website Views
 def homePage(request):
     return render(request, 'index.html')
